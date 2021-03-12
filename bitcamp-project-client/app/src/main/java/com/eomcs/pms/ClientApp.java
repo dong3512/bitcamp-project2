06df58a523301f9e2a166c282d36669452d1b6cf
dong3512 <dlehddls1015@naver.com>
2021-03-12 19:51:59 +0900

    .

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/ClientApp.java
@@ -1,24 +1,12 @@
 package com.eomcs.pms;
 
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import com.eomcs.driver.Statement;
-import com.eomcs.pms.handler.BoardAddHandler;
-import com.eomcs.pms.handler.BoardDeleteHandler;
-import com.eomcs.pms.handler.BoardDetailHandler;
-import com.eomcs.pms.handler.BoardListHandler;
-import com.eomcs.pms.handler.BoardSearchHandler;
-import com.eomcs.pms.handler.BoardUpdateHandler;
-import com.eomcs.pms.handler.Command;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
 import com.eomcs.util.Prompt;
 
 public class ClientApp {
-
-  // 사용자가 입력한 명령을 저장할 컬렉션 객체 준비
-  ArrayDeque<String> commandStack = new ArrayDeque<>();
-  LinkedList<String> commandQueue = new LinkedList<>();
 
   String serverAddress;
   int port;
@@ -34,97 +22,50 @@ public class ClientApp {
   }
 
   public void execute() {
-
-    // 사용자 명령을 처리하는 객체를 맵에 보관한다.
-    HashMap<String,Command> commandMap = new HashMap<>();
-
-    commandMap.put("/board/add", new BoardAddHandler());
-    commandMap.put("/board/list", new BoardListHandler());
-    commandMap.put("/board/detail", new BoardDetailHandler());
-    commandMap.put("/board/update", new BoardUpdateHandler());
-    commandMap.put("/board/delete", new BoardDeleteHandler());
-    commandMap.put("/board/search", new BoardSearchHandler());
-    //
-    //    commandMap.put("/member/add", new MemberAddHandler());
-    //    commandMap.put("/member/list", new MemberListHandler());
-    //    commandMap.put("/member/detail", new MemberDetailHandler());
-    //    commandMap.put("/member/update", new MemberUpdateHandler());
-    //    commandMap.put("/member/delete", new MemberDeleteHandler());
-    //
-    //    commandMap.put("/project/add", new ProjectAddHandler());
-    //    commandMap.put("/project/list", new ProjectListHandler());
-    //    commandMap.put("/project/detail", new ProjectDetailHandler());
-    //    commandMap.put("/project/update", new ProjectUpdateHandler());
-    //    commandMap.put("/project/delete", new ProjectDeleteHandler());
-    //
-    //    commandMap.put("/task/add", new TaskAddHandler());
-    //    commandMap.put("/task/list", new TaskListHandler());
-    //    commandMap.put("/task/detail", new TaskDetailHandler());
-    //    commandMap.put("/task/update", new TaskUpdateHandler());
-    //    commandMap.put("/task/delete", new TaskDeleteHandler());
-
-    try (// 서버와 통신하는 것을 대행해 줄 객체를 준비한다.
-        Statement stmt = new Statement(serverAddress, port)) {
+    try (Socket socket = new Socket(this.serverAddress, this.port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())){
 
       while (true) {
+        String message = Prompt.inputString("명령> ");
 
-        String command = com.eomcs.util.Prompt.inputString("명령> ");
+        out.writeUTF(message);
+        out.writeInt(3);
+        out.writeUTF("aaa");
+        out.writeUTF("bbb");
+        out.writeUTF("ccc");
 
-        if (command.length() == 0) {
-          continue;
-        }
+        out.flush();
 
-        // 사용자가 입력한 명령을 보관해둔다.
-        commandStack.push(command);
-        commandQueue.offer(command);
+        String response = in.readUTF();
+        int length = in.readInt();
 
-        try {
-          switch (command) {
-            case "history":
-              printCommandHistory(commandStack.iterator());
-              break;
-            case "history2": 
-              printCommandHistory(commandQueue.iterator());
-              break;
-            case "quit":
-            case "exit":
-              stmt.executeUpdate("quit");
-              System.out.println("안녕!");
-              return;
-            default:
-              Command commandHandler = commandMap.get(command);
+        ArrayList<String> data = null;
 
-              if (commandHandler == null) {
-                System.out.println("실행할 수 없는 명령입니다.");
-              } else {
-                commandHandler.service(stmt);
-              }
+        if (length > 0) {
+          data = new ArrayList<>();
+          for (int i = 0; i < length; i++) {
+            data.add(in.readUTF());
           }
-        } catch (Exception e) {
-          System.out.println("------------------------------------------");
-          System.out.printf("명령어 실행 중 오류 발생: %s\n", e.getMessage());
-          System.out.println("------------------------------------------");
         }
-        System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
-      }
+        System.out.println("----------------------------");
+        System.out.printf("작업 결과: %s\n", response);
+        System.out.printf("데이터 개수: %d\n", length);
+        if (data != null) {
+          System.out.println("데이터:");
+          for (String str : data) {
+            System.out.println(str);
+          }
+        }
 
-    } catch (Exception e) {
-      System.out.println("서버와 통신 하는 중에 오류 발생!");
-    }
-
-    Prompt.close();
-  }
-
-  private void printCommandHistory(Iterator<String> iterator) {
-    int count = 0;
-    while (iterator.hasNext()) {
-      System.out.println(iterator.next());
-      if ((++count % 5) == 0) {
-        String input = Prompt.inputString(": ");
-        if (input.equalsIgnoreCase("q")) {
+        if (message.equals("quit")) {
           break;
         }
       }
+      Prompt.close();
+
+    } catch(Exception e) {
+      System.out.println("서버와 통신 하는 중에 오류 발생!");
     }
   }
 }
