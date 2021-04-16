2eae3cfb4eb9f9a7a5f84c56c428e40dd735eaa8
dong3512 <dlehddls1015@naver.com>
2021-04-16 15:29:17 +0900

    .

--- bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/ServerApp.java
@@ -5,10 +5,14 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import com.eomcs.util.concurrent.ThreadPool;
 
 public class ServerApp {
 
   int port;
+
+  // 서버의 상태를 설정
+  boolean isStop;
 
   public static void main(String[] args) {
     ServerApp app = new ServerApp(8888);
@@ -20,6 +24,10 @@ public class ServerApp {
   }
 
   public void service() {
+
+    // 스레드풀 준비
+    ThreadPool threadPool = new ThreadPool();
+
     // 클라이언트 연결을 기다리는 서버 소켓 생성
     try (ServerSocket serverSocket = new ServerSocket(this.port)) {
 
@@ -27,13 +35,23 @@ public class ServerApp {
 
       while (true) {
         Socket socket = serverSocket.accept();
-        new Thread(() -> processRequest(socket)).start();
+        if (isStop) { // 서버의 상태가 종료이면,
+          break; // 즉시 반복문을 탈출하여 main 스레드의 실행을 끝낸다. 
+        }
+
+        threadPool.execute(() -> processRequest(socket));
       }
 
     } catch (Exception e) {
       System.out.println("서버 실행 중 오류 발생!");
       e.printStackTrace();
     }
+
+    //스레드풀의 모든 스래드를 종료시킨다.
+    // => 단 현재접속중인 스레드에 대해서는 작업을 완료 할때까지 기다린다.
+    threadPool.shutdown();
+
+    System.out.println("서버 종료!");
   }
 
   public void processRequest(Socket socket) {
@@ -47,12 +65,21 @@ public class ServerApp {
         // 클라이언트가 보낸 요청을 읽는다.
         String requestLine = in.readLine();
 
+        if (requestLine.equalsIgnoreCase("serverstop")) {
+          in.readLine();// 요청의 끝을 의미하는 빈 줄을 읽는다.
+          out.println("Server stopped!");
+          out.println();
+          out.flush();
+          terminate();
+          return;
+        }
+
         if (requestLine.equalsIgnoreCase("exit") || requestLine.equalsIgnoreCase("quit")) {
-          in.readLine();
+          in.readLine(); // 요청의 끝을 의미하는 빈 줄을 읽는다.
           out.println("GoodBye!");
           out.println();
           out.flush();
-          break ;
+          return ;
         }
 
         // 클라이언트가 보낸 명령을 서버 창에 출력한다.
@@ -71,11 +98,10 @@ public class ServerApp {
 
         // 클라이언트에게 응답한다.
         out.println("OK");
-        out.printf("당신이 요청한 명령은 %s입니다.\n", requestLine);
+        out.printf("===> %s\n", requestLine);
         out.println();
         out.flush();
       }
-
 
     } catch (Exception e) {
       System.out.println("클라이언트의 요청을 처리하는 중에 오류 발생!");
@@ -83,4 +109,19 @@ public class ServerApp {
     }
   }
 
+  // 서버를 최정적으로 종료하는 일을 한다.
+  private void terminate() {
+    // 서버 상태를 종료로 설정한다.
+    isStop = true;
+
+    // 그리고 서버가 종료할 수 있도록 임의의 접속을 수행한다.
+    // => 스스로 클라이언트가 되어 ServerSocket 에 접속하면
+    //    accept()에서 리턴하기 때문에 isStop 변수의 상태에 따라 반복문을 멈출 것이다.
+    try (Socket socket = new Socket("localhost", 8888)){
+      // 서버를 종료시키기 위해  임의로 접속하는 것이기 때문에 특별히 추가로 해야할 일이 없다.
+    } catch (Exception e){
+
+
+    }
+  }
 }
