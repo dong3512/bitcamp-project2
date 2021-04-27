a7eb1e4b73480c5edf067a671d9250891247988f
dong3512 <dlehddls1015@naver.com>
2021-04-27 11:58:04 +0900

    .

---
+++ bitcamp-project-server/app/src/main/java/com/eomcs/pms/web/UserInfoHandler.java
@@ -0,0 +1,41 @@
+package com.eomcs.pms.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.eomcs.pms.domain.Member;
+import com.eomcs.pms.service.MemberService;
+
+@WebServlet("/userInfo")
+public class UserInfoHandler extends HttpServlet {
+
+  @Override
+  public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException , IOException{
+
+    MemberService memberService = (MemberService) request.getServletContext().getAttribute("memberService");
+
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    Member member = (Member) request.getSession().getAttribute("loginUser");
+    if (member == null) {
+      out.println("로그인 하지 않았습니다!");
+      return;
+    }
+
+    out.printf("사용자번호: %d\n", member.getNo());
+    out.printf("이름: %s\n", member.getName());
+    out.printf("이메일: %s\n", member.getEmail());
+    out.printf("사진: %s\n", member.getPhoto());
+  }
+}
+
+
+
+
+
+
